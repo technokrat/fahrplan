fb486209764a01f3cdee476b722756d6eae9e795
Markus Wegmann <atokulus@gmail.com>
2013-02-21 22:19:41 +0100

    fix for the fix

--- zvv.js
+++ zvv.js
@@ -227,7 +227,7 @@ $(document).ready(function(){
           sortList(journeyids, updatedjourneyids.slice(0,maxjourneys));
 
           $.each($('.row'),function(index,val) {
-            if ($.inArray(val.attr('id'),updatedjourneyids.slice(0,maxjourneys)) == -1) {
+            if ($.inArray($(val).attr('id'),updatedjourneyids.slice(0,maxjourneys)) == -1) {
               var hideOldRow = function(){ $('#' + val).addClass('row-old').bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){ $(this).remove(); }); };
               setTimeout(hideOldRow, slideUpDelay);
               delete journeyids[index];
