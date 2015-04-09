93c472bf0f17ef27abc00d2b5efde507884bbd1a
Markus Wegmann <atokulus@gmail.com>
2015-04-09 20:19:58 +0200

    Add public files

--- TramBam.js
+++ TramBam.js
@@ -4,11 +4,16 @@ Stations = new Mongo.Collection("stations");
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('station_name', "");
-
-  Template.body.helpers({
-    station_name: function () {
-      return Session.get('station_name');
-    }
+  
+  Meteor.startup(function(){
+    Template.body.helpers({
+      station_name: function () {
+        return Session.get('station_name');
+      },
+      failure: function () {
+        return !Meteor.status().connected;
+      }
+    });
   });
 }
 
